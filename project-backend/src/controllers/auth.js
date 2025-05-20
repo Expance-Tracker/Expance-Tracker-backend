24c5b148da9d4d3ec694f0508e7802f8e1e1f123
Volodymyr Derhilov <v.dergilov@gmail.com>
2025-05-20 14:50:49 +0300

    modified returned data after login

--- project-backend/src/controllers/auth.js
+++ project-backend/src/controllers/auth.js
@@ -11,23 +11,24 @@ export const registerUserController = async (req, res) => {
 };
 
 export const loginUserController = async (req, res) => {
-  const session = await loginUser(req.body);
+  const { newSession, user } = await loginUser(req.body);
 
-  res.cookie('sessionId', session._id, {
+  res.cookie('sessionId', newSession._id, {
     httpOnly: true,
-    expires: session.refreshTokenValidUntil,
+    expires: newSession.refreshTokenValidUntil,
   });
 
-  res.cookie('refreshToken', session.refreshToken, {
+  res.cookie('refreshToken', newSession.refreshToken, {
     httpOnly: true,
-    expires: session.refreshTokenValidUntil,
+    expires: newSession.refreshTokenValidUntil,
   });
 
   res.status(200).json({
     status: 200,
     message: 'Successfully logged in an user!',
     data: {
-      accessToken: session.accessToken,
+      accessToken: newSession.accessToken,
+      ...user,
     },
   });
 };
