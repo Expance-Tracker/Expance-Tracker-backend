8f3cb68a7da2bbb6d5ecb70e93cffef27890e29e
Bibosss <savoniksasha7@gmail.com>
2025-05-20 21:41:03 +0300

    Update transactionsService.js

--- project-backend/src/services/transactionsService.js
+++ project-backend/src/services/transactionsService.js
@@ -1,14 +1,3 @@
-// import mongoose from 'mongoose';
-// import { Transaction } from '../db/models/transactionModel.js';
-
-// export const getTransactions = (userId) => {
-//   return Transaction.find({ userId: new mongoose.Types.ObjectId(userId) });
-// };
-
-
-
-
-// services/transactionsService.js
 import { Transaction } from '../db/models/transactionModel.js';
 import mongoose from 'mongoose';
 
