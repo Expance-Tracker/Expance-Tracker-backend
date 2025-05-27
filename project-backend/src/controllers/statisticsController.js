b80249693d935cd38fa114e9ee3b92dd47950c25
vanyaandrienko <vanyaandrienko@gmail.com>
2025-05-27 22:12:55 +0300

    add new format

--- project-backend/src/controllers/statisticsController.js
+++ project-backend/src/controllers/statisticsController.js
@@ -2,14 +2,14 @@ import { getPeriodSummary } from '../services/statisticsService.js';
 
 export const getPeriodSummaryController = async (req, res, next) => {
   try {
-    const { month } = req.query; // формат: YYYY-MM
+    const { month } = req.query; // формат: MM-YYYY
     const userId = req.user._id;
 
-    if (!month || !/^\d{4}-\d{2}$/.test(month)) {
-      return res.status(400).json({ message: 'Invalid month format. Use YYYY-MM.' });
+    if (!month || !/^\d{2}-\d{4}$/.test(month)) {
+      return res.status(400).json({ message: 'Invalid month format. Use MM-YYYY.' });
     }
 
-    const [year, monthNum] = month.split('-');
+    const [monthNum, year] = month.split('-');
     const data = await getPeriodSummary({ userId, year: Number(year), month: Number(monthNum) });
     res.status(200).json(data);
   } catch (error) {
